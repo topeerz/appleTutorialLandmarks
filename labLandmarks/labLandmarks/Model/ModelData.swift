d37e9de633ac07013e81ae0eb848f5d1918e3f15
Jan Maluszynski <topeerz@users.noreply.github.com>
2025-01-24 18:23:42 +0100

    Adding additional screens & navigations

--- labLandmarks/labLandmarks/Model/ModelData.swift
+++ labLandmarks/labLandmarks/Model/ModelData.swift
@@ -12,6 +12,17 @@ class ModelData {
     // that is interesting example of type inference - depending upon car type 1 item or whole json will be read
     var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 
